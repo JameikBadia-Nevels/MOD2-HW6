a1cf2e7fa031ccaf6f65e45163c009fc77a8b46a
JameikBadia-Nevels <jameiknevels@gmail.com>
2022-03-09 15:31:51 -0500

    Heroku deployment

--- server.js
+++ server.js
@@ -18,6 +18,19 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.get('/', (req,res)=>{
     res.send('Welcome to the Pokemon App!')
 })
+app.get('/pokemon/seed', (req, res)=>{
+    Pokemon.create([
+        {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur"},
+        {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
+        {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
+        {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
+        {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
+        {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
+        {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"}
+    ], (err, data)=>{
+        res.redirect('/pokemon');
+    })
+});
 
 app.get('/pokemon', (req,res) => {
     Pokemon.find({}, (error,allPokemon)=>{
--- views/Show.jsx
+++ views/Show.jsx
@@ -7,13 +7,26 @@ const myStyle = {
 class Index extends React.Component{
     render(){
         const {pokemon} = this.props
+        if(pokemon.name == "bulbasaur" || pokemon.name == "ivysaur" || pokemon.name == "venusaur" || pokemon.name == "charmander" || pokemon.name == "charizard" || pokemon.name == "squirtle" || pokemon.name == "wartortle"){
+            return(
+                <div>
+                <h1 style = {myStyle}>Gotta Catch Em ALLLLLLL!</h1>
+                <h2> {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)} I Choose You</h2>
+                <br></br>
+                <div>
+                    <img src = {pokemon.img + ".jpg"} /> 
+                </div>
+                <a href = '/pokemon'> Back </a>
+                </div>
+            )
+        }
         return (
             <div>
             <h1 style = {myStyle}>Gotta Catch Em ALLLLLLL!</h1>
             <h2> {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)} I Choose You</h2>
             <br></br>
             <div>
-                <img src = {pokemon.img}/> 
+                <img src = {pokemon.img} /> 
             </div>
             <a href = '/pokemon'> Back </a>
             </div>
